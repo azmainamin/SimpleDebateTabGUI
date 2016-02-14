860fbec85815b87591fa679c4414048d2fccf2b6
Azmain Amin <mdazmainamin@gmail.com>
2016-02-14 02:17:48 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.awt.Dimension;
+/* Main Driver for the program.
+ * @author : Azmain Amin
+ * 
+ */
+
+public class Main {
+	
+	public static final Dimension SIZE = new Dimension(800,600);
+	public static final String NAME = "Debate Tab";
+	
+
+	public static void main(String[] args) {
+		Frame mainFrame = new Frame(SIZE,NAME);
+	}
+}
